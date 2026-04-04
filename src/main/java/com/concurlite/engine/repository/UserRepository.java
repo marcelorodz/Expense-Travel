45c9b6b42c1584378cf3b34a51437145b50d3c41
Marcelo Rodrigues <69660251+marcelorodz@users.noreply.github.com>
2026-04-04 11:25:18 -0300

    Rename package from concurlite to expenselite

--- src/main/java/com/concurlite/engine/repository/UserRepository.java
+++ src/main/java/com/concurlite/engine/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.concurlite.engine.repository;
+package com.expenselite.engine.repository;
 
-import com.concurlite.engine.domain.User;
+import com.expenselite.engine.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
