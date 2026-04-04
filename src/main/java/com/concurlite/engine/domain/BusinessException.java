f7703d8f8223ebed650566e62881a4581ceb3d46
Marcelo Rodrigues <69660251+marcelorodz@users.noreply.github.com>
2026-04-04 11:19:17 -0300

    Change package name from concurlite to expenselite

--- src/main/java/com/concurlite/engine/domain/BusinessException.java
+++ src/main/java/com/concurlite/engine/domain/BusinessException.java
@@ -1,4 +1,4 @@
-package com.concurlite.engine.domain;
+package com.expenselite.engine.domain;
 
 public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
